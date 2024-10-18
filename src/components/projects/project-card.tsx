98bff0b3a654293afc422ec2c4f4be4100ec3ea5
Erivelto Silva <eriveltoclenio@gmail.com>
2024-10-18 01:42:14 -0300

    refactor: new truncate-line-2

--- src/components/projects/project-card.tsx
+++ src/components/projects/project-card.tsx
@@ -9,10 +9,9 @@ export const ProjectCard = ({ project }: { project: ProjectEntity }) => {
     <article className="relative group bg-white w-full h-72 rounded-lg shadow-md overflow-hidden">
       <Image src={project?.image} alt={project?.title} className="w-full h-full" width={0} height={0} sizes="100vw" />
       {/* Content showed on mouse hover */}
-      <div className="h-2/5 absolute bottom-0 left-0 right-0 bg-gray-50 p-4 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 ease-in-out">
-        <div className="flex justify-between">
-          <h3 className="text-base lg:text-xl font-semibold text-primary">{project.title}</h3>
-          <div className="flex flex-wrap gap-2">
+      <div className="h-2/5 absolute bottom-0 left-0 right-0 bg-gray-50 py-1 px-4 transform translate-y-full group-hover:translate-y-0 transition-transform duration-500 ease-in-out">
+        <div className="flex flex-col">
+          <div className="flex w-full justify-end flex-wrap gap-2">
             {project?.linkURL && (
               <MediaLink2
                 Icon={BiGlobe}
@@ -46,8 +45,11 @@ export const ProjectCard = ({ project }: { project: ProjectEntity }) => {
               />
             )}
           </div>
+          <h3 className="text-base lg:text-xl font-semibold text-primary">{project.title}</h3>
         </div>
-        <p className="my-2 text-gray-600 text-xs lg:text-sm truncate-lines truncate-lines-3">{project.description}</p>
+        <p className="my-2 text-gray-600 text-[8px] lg:text-xs truncate-lines truncate-lines-2 mb-4 flex-grow">
+          {project.description}
+        </p>
       </div>
     </article>
   );
